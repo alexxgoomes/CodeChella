3e2f1b94c4ed063e4b36f407c127bf1fc3e8f1c8
Alex Gomes <alexsgomes0704@gmail.com>
2023-03-30 08:59:33 -0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const form = document.getElementById("formulario");
+
+form.addEventListener("subimit", (evento) => {
+    const nome = evento.target.elements['nome'];
+    
+})
+
+function criaElemento(nome) {
+
+}
