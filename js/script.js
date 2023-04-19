739e9d3ea41872d3bee65174ccdd57de9387d78d
Alex Gomes <alexsgomes0704@gmail.com>
2023-04-19 11:34:41 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,84 @@
-import ehUmCPF from "./valida-cpf";
-const camposDoFormulario = document.querySelectorAll("[required]");
+import ehUmCPF from "./valida-cpf.js";
+import ehMaiorDeIdade from "./valida-idade.js";
+const camposDoFormulario = document.querySelectorAll('[required]');
+const formulario = document.querySelector("[data-formulario]");
 
-camposDoFormulario.forEach((campo) => {
-    campo.addEventListener("blur", () => verificaCampo(campo));
+//Para não dar reload quando dá o Submit
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const listaRespostas = {
+        "nome": e.target.elements["nome"].value,
+        "email": e.target.elements["email"].value,
+        "cpf": e.target.elements["cpf"].value,
+        "aniversario": e.target.elements["aniversario"].value,
+        "tipo": e.target.elements["tipo"].value
+    };
+
+    localStorage.setItem("cadastro", JSON.stringify(listaRespostas));
+
+    window.location.href= './ticket.html';
 })
 
+camposDoFormulario.forEach((campo) => {
+    campo.addEventListener("blur", () => verificaCampo(campo))
+    campo.addEventListener("invalid", evento => evento.preventDefault())
+});
+
+const tiposDeErro = [
+    'valueMissing',
+    'typeMismatch',
+    'patternMisMatch',
+    'tooShort',
+    'customError'
+];
+
+const mensagens = {
+    nome: {
+        valueMissing: "O campo de nome não pode estar vazio.",
+        patternMismatch: "Por favor, preencha um nome válido.",
+        tooShort: "Por favor, preencha um nome válido."
+    },
+    email: {
+        valueMissing: "O campo de e-mail não pode estar vazio.",
+        typeMismatch: "Por favor, preencha um email válido.",
+        tooShort: "Por favor, preencha um e-mail válido."
+    },
+    cpf: {
+        valueMissing: 'O campo de CPF não pode estar vazio.',
+        patternMismatch: "Por favor, preencha um CPF válido.",
+        customError: "O CPF digitado não existe.",
+        tooShort: "O campo de CPF não tem caractéres suficientes."
+    },
+    aniversario: {
+        valueMissing: 'O campo de data de nascimento não pode estar vazio.',
+        customError: 'Você deve ser maior que 16 anos para se cadastrar.'
+    }
+};
+
 function verificaCampo(campo) {
-    if(campo.value == "cpf" && campo.value.lenght >= 11){
+    let mensagem = "";
+    campo.setCustomValidity ('');
+    if(campo.name == "cpf" && campo.value.length >= 11) {
         ehUmCPF(campo);
+    }
+    if(campo.name == "aniversario" && campo.value != "") {
+        ehMaiorDeIdade(campo);
+    }
+    console.log(campo.validity);
+
+    tiposDeErro.forEach(erro => {
+        if (campo.validity[erro]) {
+            mensagem = mensagens[campo.name][erro];
+            console.log(mensagem);
+        }
+    })
+    const mensagemErro = campo.parentNode.querySelector('.mensagem-erro');
+    const validadorDeInput = campo.checkValidity();
+
+    if (!validadorDeInput) {
+        mensagemErro.textContent = mensagem;
+    } else {
+        mensagemErro.textContent = "";
     }
 }
