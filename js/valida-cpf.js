a57b04818a43dafee78bac4d79c2fde187000d4e
Alex Gomes <alexsgomes0704@gmail.com>
2023-04-19 11:34:43 -0300

    Update valida-cpf.js

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,6 +1,8 @@
 export default function ehUmCPF(campo) {
     const cpf = campo.value.replace(/\.|-/g, "");
-    console.log(validaNumerosRepetidos(cpf));
+    if(validaNumerosRepetidos(cpf) || validaPrimeiroNumero(cpf) || validaSegundoNumero(cpf)){
+        campo.setCustomValidity('Esse CPF não é válido');
+    } 
 }
 
 function validaNumerosRepetidos(cpf) {
@@ -15,6 +17,41 @@ function validaNumerosRepetidos(cpf) {
         '77777777777',
         '88888888888',
         '99999999999'
-    ];
-    return validaNumerosRepetidos(cpf);
+    ]
+
+    return numerosRepetidos.includes(cpf);
+}
+
+function validaPrimeiroNumero(cpf) {
+    let soma = 0;
+    let multiplicador = 10;
+
+    for(let tamanho = 0; tamanho < 9; tamanho++){
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+
+    soma = (soma * 10) % 11;
+
+    if(soma == 10 || soma == 11){
+        soma = 0;
+    }
+     return soma != cpf[9];
+}
+
+function validaSegundoNumero(cpf) {
+    let soma = 0;
+    let multiplicador = 11;
+
+    for(let tamanho = 0; tamanho < 10; tamanho++){
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+
+    soma = (soma * 10) % 11;
+
+    if(soma == 10 || soma == 11){
+        soma = 0;
+    }
+     return soma != cpf[10];
 }
